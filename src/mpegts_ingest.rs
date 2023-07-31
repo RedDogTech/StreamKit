966b57f5e8731c57390f79c7e6d4c6199dbc91af
Luke Moscrop <luke.moscrop@itv.com>
2023-07-31 10:02:50 +0100

    Fix temp workaround for first pcr being wrong

--- src/mpegts_ingest.rs
+++ src/mpegts_ingest.rs
@@ -1,21 +1,20 @@
-use std::ops::Add;
-
-use chrono::{DateTime, Utc, Duration};
-use float_duration::FloatDuration;
 use mpegts::{demuxer::Demuxer, pid::Pid, stream_type::StreamType};
+use time::{OffsetDateTime, Duration};
 
 pub struct IngestDemuxer {
     latest_pcr_value: i64,
     latest_pcr_timestamp_90khz: u64,
-    latest_pcr_datetime: DateTime<Utc>,
+    latest_pcr_datetime:  OffsetDateTime,
+    first_pcr: bool,
 }
 
 impl Default for IngestDemuxer {
     fn default() -> Self {
         IngestDemuxer {
+            first_pcr: false,
             latest_pcr_value: 0,
             latest_pcr_timestamp_90khz: 0,
-            latest_pcr_datetime: (Utc::now() - Duration::seconds(1)),
+            latest_pcr_datetime: (OffsetDateTime::now_utc()),
         }
     }
 }
@@ -34,12 +33,20 @@ impl mpegts::DemuxerEvents for IngestDemuxer {
     }
 
     fn on_pcr(&mut self, pcr: u64) {
-        let prc_value: i64 = (pcr as i64 - mpegts::HZ as i64 + mpegts::PCR_CYCLE as i64) % mpegts::PCR_CYCLE as i64;
-        let pcr_diff = (prc_value - self.latest_pcr_value + mpegts::PCR_CYCLE as i64) % mpegts::PCR_CYCLE as i64;
 
-        self.latest_pcr_value = prc_value;
-        self.latest_pcr_timestamp_90khz += pcr_diff as u64;
-        self.latest_pcr_datetime += FloatDuration::seconds(pcr_diff as f64 / mpegts::HZ as f64).to_chrono().unwrap();
+        let prc_value: i64 = (pcr as i64 - mpegts::HZ as i64 + mpegts::PCR_CYCLE as i64) % mpegts::PCR_CYCLE as i64;
+        
+        // FIXME: this could be better :(
+        if self.first_pcr {
+            let pcr_diff = (prc_value - self.latest_pcr_value + mpegts::PCR_CYCLE as i64) % mpegts::PCR_CYCLE as i64;
+            self.latest_pcr_timestamp_90khz += pcr_diff as u64;
+            self.latest_pcr_datetime += Duration::seconds_f64(pcr_diff as f64 / mpegts::HZ as f64);
+
+            println!("{}", self.latest_pcr_datetime);
+        }
+        
+        self.first_pcr = true;
+        self.latest_pcr_value = prc_value;   
     }
 }
 
