2bed58abbf271fcf7a6933723073ff54b4d785c9
Luke Moscrop <luke.moscrop@itv.com>
2023-07-31 09:24:36 +0100

    fix pcr maths calculations

--- src/mpegts_ingest.rs
+++ src/mpegts_ingest.rs
@@ -1,16 +1,23 @@
+use std::ops::Add;
+
+use chrono::{DateTime, Utc, Duration};
+use float_duration::FloatDuration;
 use mpegts::{demuxer::Demuxer, pid::Pid, stream_type::StreamType};
 
-
-
+pub struct IngestDemuxer {
+    latest_pcr_value: i64,
+    latest_pcr_timestamp_90khz: u64,
+    latest_pcr_datetime: DateTime<Utc>,
+}
 
 impl Default for IngestDemuxer {
     fn default() -> Self {
         IngestDemuxer {
+            latest_pcr_value: 0,
+            latest_pcr_timestamp_90khz: 0,
+            latest_pcr_datetime: (Utc::now() - Duration::seconds(1)),
         }
     }
-}
-
-pub struct IngestDemuxer {
 }
 
 impl mpegts::DemuxerEvents for IngestDemuxer {
@@ -26,8 +33,13 @@ impl mpegts::DemuxerEvents for IngestDemuxer {
 
     }
 
-    fn on_pcr(&mut self, clock: u64) {
-        log::info!("New clock {clock}");
+    fn on_pcr(&mut self, pcr: u64) {
+        let prc_value: i64 = (pcr as i64 - mpegts::HZ as i64 + mpegts::PCR_CYCLE as i64) % mpegts::PCR_CYCLE as i64;
+        let pcr_diff = (prc_value - self.latest_pcr_value + mpegts::PCR_CYCLE as i64) % mpegts::PCR_CYCLE as i64;
+
+        self.latest_pcr_value = prc_value;
+        self.latest_pcr_timestamp_90khz += pcr_diff as u64;
+        self.latest_pcr_datetime += FloatDuration::seconds(pcr_diff as f64 / mpegts::HZ as f64).to_chrono().unwrap();
     }
 }
 
